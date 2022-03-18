5efa9e3547ca660481eb3f0f617a38e6db1d98b0
robin marillia <robin4a4@gmail.com>
2022-03-18 16:28:06 +0100

    refactor: prettier

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -84,21 +84,33 @@ export default function ImageModal(props) {
                   </button>
                 </div>
                 <div class="relative flex items-center justify-center">
-                    <div class="z-1 rounded-md bg-neutral-900 bg-opacity-25 flex items-center justify-center flex-1 h-full top-O bottom-O right-0 left-0 absolute">
-                        <svg class="animate-spin h-12 w-12" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                        viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                            <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                            </path>
-                        </svg>
-                    </div>
-                    <img
+                  <div class="z-1 rounded-md bg-neutral-900 bg-opacity-25 flex items-center justify-center flex-1 h-full top-O bottom-O right-0 left-0 absolute">
+                    <svg
+                      class="animate-spin h-12 w-12"
+                      version="1.1"
+                      id="L9"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 100 100"
+                      enable-background="new 0 0 0 0"
+                      xml:space="preserve"
+                    >
+                      <path
+                        fill="#fff"
+                        d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+                      ></path>
+                    </svg>
+                  </div>
+                  <img
                     loading="lazy"
                     src={props.image}
                     class="rounded-md relative z-2"
                     alt={props.description ? props.description : ""}
                     width={props.width}
                     height={props.height}
-                    />
+                  />
                 </div>
                 {props.description && props.date && (
                   <div class="flex gap-2 items-center pt-2">
